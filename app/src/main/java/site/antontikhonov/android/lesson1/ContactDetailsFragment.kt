02b9763eab41e14f0c3ae9b590a1ebd6577c2c7f
Anton Tikhonov <free.tikhonov@gmail.com>
2021-03-13 14:37:41 +0400

    add bound service

--- app/src/main/java/site/antontikhonov/android/lesson1/ContactDetailsFragment.kt
+++ app/src/main/java/site/antontikhonov/android/lesson1/ContactDetailsFragment.kt
@@ -1,15 +1,29 @@
 package site.antontikhonov.android.lesson1
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 private const val EXTRA_CONTACT_ID = "CONTACT_ID"
 
 class ContactDetailsFragment : Fragment() {
+
+    private var serviceInterface: ContactService.ServiceInterface? = null
+    private var contactId: Int = 0
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is ContactService.ServiceInterface) {
+            serviceInterface = context
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,7 +35,32 @@ class ContactDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.contact_details_title)
-        val id = arguments?.getInt(EXTRA_CONTACT_ID)
+        contactId = arguments?.getInt(EXTRA_CONTACT_ID) ?: throw IllegalArgumentException("Contact ID required")
+        loadContactById()
+    }
+
+    fun loadContactById() = serviceInterface?.getService()?.getContactById(callback, contactId)
+
+    private val callback = object : ResultListener {
+        override fun onComplete(result: Contact) {
+            view?.post(Runnable {
+                val nameTextView = view?.findViewById<TextView>(R.id.contactNameDetails)
+                val phoneNumTextView = view?.findViewById<TextView>(R.id.contactNum1)
+                val phoneNum2TextView = view?.findViewById<TextView>(R.id.contactNum2)
+                val emailTextView = view?.findViewById<TextView>(R.id.contactEmail1)
+                val email2TextView = view?.findViewById<TextView>(R.id.contactEmail2)
+                val descriptionTextView = view?.findViewById<TextView>(R.id.contactDescription)
+                val imageView = view?.findViewById<ImageView>(R.id.contactImageDetails)
+
+                nameTextView?.text = result.name
+                phoneNumTextView?.text = result.firstNum
+                phoneNum2TextView?.text = result.secondNum
+                emailTextView?.text = result.firstEmail
+                email2TextView?.text = result.secondEmail
+                descriptionTextView?.text = result.description
+                imageView?.setImageDrawable(activity?.let { ContextCompat.getDrawable(it.applicationContext, result.image) })
+            })
+        }
     }
 
     companion object {
@@ -32,5 +71,14 @@ class ContactDetailsFragment : Fragment() {
             fragment.arguments = args
             return fragment
         }
+    }
+
+    override fun onDetach() {
+        serviceInterface = null
+        super.onDetach()
+    }
+
+    interface ResultListener {
+        fun onComplete(result: Contact)
     }
 }
--- app/src/main/java/site/antontikhonov/android/lesson1/MainActivity.kt
+++ app/src/main/java/site/antontikhonov/android/lesson1/MainActivity.kt
@@ -1,33 +1,70 @@
 package site.antontikhonov.android.lesson1
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import java.lang.ref.WeakReference
 
-private const val CONTACT_ID : Int = 7
+private const val CONTACT_ID = 0
+private const val TAG = "fragmentTag"
 
-class MainActivity : AppCompatActivity(), ContactListFragment.FragmentClickListener {
+class MainActivity : AppCompatActivity(),
+    ContactListFragment.ContactListListener,
+    ContactService.ServiceInterface {
+
+    private var contactService: ContactService? = null
+    private var bound = false
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName?) {
+            bound = false
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder = service as ContactService.ContactBinder
+            contactService = binder.getService()
+            bound = true
+            val weakReferenceFragment = WeakReference(supportFragmentManager.findFragmentByTag(TAG))
+            when (val fragment = weakReferenceFragment.get()) {
+                is ContactListFragment -> fragment.loadContacts()
+                is ContactDetailsFragment -> fragment.loadContactById()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if(savedInstanceState == null) {
-            addContactListFragment()
+            supportFragmentManager.beginTransaction()
+                    .add(R.id.fragments_container, ContactListFragment(), TAG)
+                    .commit()
         }
+        val intent = Intent(this, ContactService::class.java)
+        bindService(intent, connection, Context.BIND_AUTO_CREATE)
     }
 
-    private fun addContactListFragment() {
-        supportFragmentManager.beginTransaction()
-            .add(R.id.fragments_container, ContactListFragment())
-            .commit()
-    }
-
-    private fun addContactDetailsFragment() {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragments_container, ContactDetailsFragment.newInstance(CONTACT_ID))
-            .addToBackStack(null)
-            .commit()
+    override fun onDestroy() {
+        if(bound) {
+            unbindService(connection)
+            bound = false
+        }
+        contactService = null
+        super.onDestroy()
     }
 
     override fun onClickFragment() {
-        addContactDetailsFragment()
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.fragments_container, ContactDetailsFragment.newInstance(CONTACT_ID), TAG)
+                .addToBackStack(null)
+                .commit()
+    }
+
+    override fun getService() : ContactService? {
+        return contactService
     }
 }
